e7a1c69eaaca0b389837b7bd8d6ddbf890378381
Kenny Woodson <kwoodson@redhat.com>
2018-12-21 10:27:12 -0500

    advent day 2 part 2

--- advents/day2/day2.go
+++ advents/day2/day2.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func getData(fileName string) ([]string, error) {
@@ -68,11 +69,54 @@ func part1(lines []string) int {
 	return total
 }
 
+// https://stackoverflow.com/questions/24893624/how-to-replace-a-letter-at-a-specific-index-in-a-string-in-go
+func removeFromString(in string, removeList []int) string {
+	var sb strings.Builder
+	modified := []rune(in)
+	for _, num := range removeList {
+		for idx, r := range modified {
+			if num != idx {
+				sb.WriteRune(r)
+			}
+		}
+	}
+
+	return sb.String()
+}
+
+func part2(lines []string) string {
+	// similarStrings := make([]string, 20)
+	for i, original := range lines {
+		for _, compareto := range lines[i:] {
+			differences := make([]int, 0)
+			// compare the two strings
+			rOriginal := []rune(original)
+			rCompareto := []rune(compareto)
+			for x := 0; x <= len(rOriginal)-1; x++ {
+				if rOriginal[x] != rCompareto[x] {
+					differences = append(differences, x)
+				}
+				if len(differences) > 1 {
+					// fmt.Printf("Differences > 1: [%v]\n", differences)
+					break
+				}
+			}
+			if len(differences) == 1 {
+				return removeFromString(original, differences)
+			}
+		}
+	}
+
+	return ""
+}
+
 func main() {
 	dataFileName := "../../data/day2/day_2_input.txt"
 	lines, err := getData(dataFileName)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(part1(lines))
+	// fmt.Println(part1(lines))
+
+	fmt.Println(part2(lines))
 }
